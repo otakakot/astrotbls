1feef05089080056e4199b1958256c57313f357a
otakakot <kotaro.otaka@gmail.com>
2023-10-28 15:09:08 +0900

    first commit

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    base: '/doc',
     markdown: {
         shikiConfig: {
             theme: 'dark-plus',
