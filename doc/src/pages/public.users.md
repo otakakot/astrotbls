9d321e4c6df775477d7112c0bb61c8a19617a8a2
otakakot <kotaro.otaka@gmail.com>
2023-10-28 14:30:31 +0900

    first commit

--- doc/src/pages/public.users.md
+++ doc/src/pages/public.users.md
@@ -1,3 +1,7 @@
+---
+layout: ../layouts/Layout.astro
+---
+
 # public.users
 
 ## Description
--- script/doc.sh
+++ script/doc.sh
@@ -21,4 +21,25 @@ do
     sed -i '' -e 's/(\([a-zA-Z0-9_.-]*\.svg\))/(\.\/\1)/g' "${file}"
 done
 
+# .md ファイルに以下のヘッダーを追加
+header='---
+layout: ../layouts/Layout.astro
+---
+'
+
+find "${dir}" -name '*.md' | while read file
+do
+    # 一時ファイルを作成
+    temp_file="tmpmd"
+
+    # ヘッダーを一時ファイルに書き込む
+    echo "$header" > "$temp_file"
+
+    # 元のファイルの内容を一時ファイルに追加
+    cat "$file" >> "$temp_file"
+
+    # 一時ファイルを元のファイルに移動
+    mv "$temp_file" "$file"
+done
+
 rm -rf "${tmp}"
